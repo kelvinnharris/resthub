938dee42e4eb7bbd1bd303ec919018259957e662
kelvinnharris <kelvinnharris@gmail.com>
2021-09-12 21:00:20 +0800

    Fix multiple db connect calls

--- db.js
+++ db.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-if (process.env.NODE_ENV == 'test') 
-    const mongodb_uri = 'mongodb://localhost/resthub';
-else
-    const mongodb_uri = process.env.MONGODB_URI;
+var mongodb_uri = '';
+if (process.env.NODE_ENV == 'test' || process.env.NODE_ENV == 'dev') {
+    mongodb_uri = 'mongodb://localhost/resthub';
+} else {
+    mongodb_uri = process.env.MONGODB_URI;
+}
 
 
 function connect() {
@@ -15,26 +17,26 @@ function connect() {
         useUnifiedTopology: true
     };
 
-    if (process.env.NODE_ENV == 'test') {
-        console.log('test environment');
-        const Mockgoose = require('mockgoose').Mockgoose;
-        const mockgoose = new Mockgoose(mongoose);
-        mockgoose.prepareStorage()
-            .then(() => {
-                mongoose.connect(mongodb_uri, options)
-                    .then((res, err) => {
-                        if (err) return reject(err);
-                        resolve();
-                    });
-            });
-    }
-
     return new Promise((resolve, reject) => {
-        mongoose.connect(mongodb_uri, options)
-            .then((res, err) => {
-                if (err) return reject(err);
-                resolve();
-            });
+        if (process.env.NODE_ENV == 'test') {
+            console.log('test environment');
+            const Mockgoose = require('mockgoose').Mockgoose;
+            const mockgoose = new Mockgoose(mongoose);
+            mockgoose.prepareStorage()
+                .then(() => {
+                    mongoose.connect(mongodb_uri, options)
+                        .then((res, err) => {
+                            if (err) return reject(err);
+                            resolve();
+                        });
+                });
+        } else {
+            mongoose.connect(mongodb_uri, options)
+                .then((res, err) => {
+                    if (err) return reject(err);
+                    resolve();
+                });
+        }
     });
 }
 
